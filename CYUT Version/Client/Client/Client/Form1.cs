412f1f6814392c08d61f983ecb0232e54d866df1
Ethon Wu <a5351998@gmail.com>
2016-08-18 22:49:47 +0800

    Paint but can't sand file

---
+++ CYUT Version/Client/Client/Client/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        Socket clientSocket=null;
+        delegate void setTextDel(String tmpStr);
+        Thread clientThread;
+       // Socket[] serverSocket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hostname;
+            hostname = Dns.GetHostName();
+            IPHostEntry host;
+            host = Dns.GetHostEntry(hostname);
+            foreach (IPAddress addrList in host.AddressList)
+            {
+
+                // Console.WriteLine( addrList);
+                comboBox1.Items.Add(addrList);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+                IPAddress serverIp = IPAddress.Parse(comboBox1.Text);
+                IPEndPoint serverhost;
+               
+                serverhost = new IPEndPoint(serverIp,int.Parse(textBox1.Text));
+                Console.WriteLine("Test:" + int.Parse(textBox1.Text)+serverIp);
+                clientSocket.Connect(serverhost);
+                Console.WriteLine("Server is connected....");
+                clientThread = new Thread(reviceThreadProc);
+                clientThread.Start();
+            
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] msg;
+            msg = Encoding.Default.GetBytes(textBox2.Text);
+            clientSocket.Send(msg, 0, msg.Length, SocketFlags.None);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        private void reviceThreadProc()
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+                int rcvBytes;
+                String tmpStr;
+                do
+                {
+                    rcvBytes = clientSocket.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+                    tmpStr = Encoding.Default.GetString(bytes, 0, rcvBytes);
+                    setText(tmpStr);
+
+                }
+                while (clientSocket.Available != 0 || rcvBytes != 0);
+        
+               
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void setText(String tmpStr)
+        {
+            if (textBox2.InvokeRequired == true)
+            {
+                setTextDel d = new setTextDel(setText);
+                Invoke(d, tmpStr);
+            }
+            else
+            {
+                textBox2.Text = tmpStr;
+            }
+        }
+        private void Form1_FormClosing(Object sender,System.Windows.Forms.FormClosingEventArgs e)
+        {
+            if (clientSocket != null)
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            if(clientThread != null)
+            {
+                clientThread.Abort();
+            }
+          
+        }
+    }
+}
---
+++ CYUT Version/Server/Server/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Host IP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(150, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            this.label2.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Listen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Accept";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(79, 41);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Receive";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Message:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 268);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(188, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 38);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Close";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(150, 134);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 39);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Send";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 319);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Server_Socket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
